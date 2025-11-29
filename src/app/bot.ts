a88cd8448e5f43193697922bc7206d2e9e3b8ee6
Clément <kyloren3600@gmail.com>
2025-11-29 22:02:14 +0100

    add log message

--- src/app/bot.ts
+++ src/app/bot.ts
@@ -3,24 +3,28 @@ import { Client } from "discord.js";
 import intents from "../lib/discord-intents.js";
 
 import CommandHandler from "./commands/command-handler.js"
-// import EventHandler from "./EventHandler"
+import logger from "./logger.js";
 
 export default class Bot {
 
 	public readonly client: Client
-    public readonly commandHandler: CommandHandler
-    // public readonly eventHandler: EventHandler
+	public readonly commandHandler: CommandHandler
 
 	constructor() {
 
 		const client = new Client({ intents: intents });
 		this.client = client
-		
+
 		this.commandHandler = new CommandHandler(this)
-		// this.eventHandler = new EventHandler(this)
+
+		client.once("ready", () => {
+			logger.info(`Logged in as ${client.user?.tag}`)
+		})
+
 	}
 
 	public login(token: string) {
+
 		this.client.login(token)
 	}
 
